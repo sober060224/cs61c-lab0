125ebf1a1889978dcd571250da1234e72ee5a7ef
sober060224 <2680038634@qq.com>
2026-01-12 10:03:43 +0800

    完成fizzbuzz函数编写

---
+++ lab00/code.py
@@ -0,0 +1,9 @@
+def fizzbuzz(num):
+    if num % 15 == 0:
+        print(f"{num}:fizzbuzz")
+    elif num % 3 == 0:
+        print(f"{num}:fizz")
+    elif num % 5 == 0:
+        print(f"{num}:buzz")
+for i in range(1, 21):
+    fizzbuzz(i)
